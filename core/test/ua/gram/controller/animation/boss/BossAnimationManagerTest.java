30a498df5c86f6b955761ba385692a9b86715796
Gram <gram7gram@gmail.com>
2016-06-28 21:08:01 +0300

    #master in progress * modified level previews and maps

---
+++ core/test/ua/gram/controller/animation/boss/BossAnimationManagerTest.java
@@ -0,0 +1,9 @@
+package ua.gram.controller.animation.boss;
+
+import ua.gram.GameTestCase;
+
+/**
+ * @author Gram <gram7gram@gmail.com>
+ */
+public class BossAnimationManagerTest extends GameTestCase {
+}
